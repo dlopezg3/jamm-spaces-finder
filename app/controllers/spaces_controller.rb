6bd2dc8867a72ecb8d11c38f05d939acad7f2169
jeremy dhanani <Jeremy.dhanani@gmail.com>
2018-02-26 16:08:33 +0000

    Spaces controller actions

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,25 +2,41 @@ class SpacesController < ApplicationController
   before_action :set_space, only: [:show, :edit, :update, :delete]
 
   def index
+    @spaces = Space.all
   end
 
   def show
   end
 
   def create
+    @space = Space.new
+    if @space.save
+      redirect_to spaces_path
+    else
+      render :new
+    end
   end
 
   def new
+    @space = Space.new
   end
 
   def edit
   end
 
   def update
+    @space.update(space_params)
+    redirect_to spaces_path
   end
 
-  def delete
+  def destroy
+    @space.destroy
+    redirect_to spaces_path
   end
+
+  def search
+  end
+
 
   private
 
