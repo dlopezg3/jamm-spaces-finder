99724edc1772d2e9285474116ffa4f30e438b68b
jeremy dhanani <Jeremy.dhanani@gmail.com>
2018-02-27 23:29:14 +0000

    navbar javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
+//= require jquery
+
 import "bootstrap";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+initUpdateNavbarOnScroll();
+
+console.log("Hello World from console");
